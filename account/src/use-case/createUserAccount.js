c962a5a0010e36158cfb3545679da572efb3154d
thayarasiqueira <thay.siqu@gmail.com>
2023-01-02 14:52:53 -0300

    feat: add function to create an object representing each user

---
+++ account/src/use-case/createUserAccount.js
@@ -0,0 +1,15 @@
+let allUsers = [];
+
+const createUserUseCase = (nome, email, senha) => {
+    allUsers.push({
+        id: allUsers.length + 1,
+        nome,
+        email,
+        senha,
+        createdDate: new Date().toISOString(),
+    })
+
+    return allUsers;
+}
+
+export default createUserUseCase;
---
+++ account/test/createUserAccount.test.js
@@ -0,0 +1,3 @@
+import createUserUseCase from "../src/use-case/createUserAccount.js";
+
+console.log(createUserUseCase('teste', 'teste@dominio.com', '123456'));
