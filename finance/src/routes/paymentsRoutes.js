ae5d85281bcd0b63e8be42538b046e6028d0030a
thayarasiqueira <thay.siqu@gmail.com>
2023-02-08 08:53:38 -0300

    feat: add status validation to PATCH /payments/:id

--- finance/src/routes/paymentsRoutes.js
+++ finance/src/routes/paymentsRoutes.js
@@ -1,12 +1,13 @@
-const { Router } = require('express')
-const PaymentsController = require('../controllers/paymentsController.js')
+const { Router } = require('express');
+const PaymentsController = require('../controllers/paymentsController.js');
+const validateStatus = require('../validations/paymentsValidation.js');
 
 const router = Router();
 
 router
     .get('/payments/:id', PaymentsController.findPaymentById)
     .post('/payments', PaymentsController.createPayment)
-    .patch('/payments/:id', PaymentsController.updateStatus)
+    .patch('/payments/:id', validateStatus, PaymentsController.updateStatus)
 
 
 module.exports = router;
---
+++ finance/src/validations/paymentsValidation.js
@@ -0,0 +1,23 @@
+const db = require('../models');
+
+const validateStatus = async (req, res, next) => {
+    const { id } = req.params;
+    try {
+      const {status} = await db.Payments.findOne( { 
+        where: { 
+          id: Number(id)
+        }
+      })
+      if (status === 'CONFIRMADO') {
+        return res.status(422).json({message: 'Status change denied!'});
+      }
+      if (status === 'CANCELADO') {
+        return res.status(422).json({message: 'Status change denied!'});
+    }
+      next();
+    } catch (error) {
+      return res.status(500).json(error.message);
+    }
+}
+
+module.exports = validateStatus;
