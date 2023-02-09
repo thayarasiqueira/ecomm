0e86e8ba150b99b6d64b07e065f6f7c9af415ea4
thayarasiqueira <thay.siqu@gmail.com>
2023-02-09 14:48:55 -0300

    local usage

--- product/src/config/dbConnect.js
+++ product/src/config/dbConnect.js
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose';
 
-//mongoose.connect('mongodb://admin:secret@127.0.0.1:27017/ecomm-product?authSource=admin')
-mongoose.connect('mongodb://admin:secret@mongodb:27017/ecomm-account?authSource=admin')
+mongoose.connect('mongodb://admin:secret@127.0.0.1:27017/ecomm-product?authSource=admin')
+//mongoose.connect('mongodb://admin:secret@mongodb:27017/ecomm-account?authSource=admin')
 const db = mongoose.connection;
 
 export default db;
