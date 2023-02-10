f6252be50ef3c2d5169b0cc2017b885730b839a4
thayarasiqueira <thay.siqu@gmail.com>
2023-02-10 14:46:22 -0300

    fix: bind services' ports

--- product/src/config/dbConnect.js
+++ product/src/config/dbConnect.js
@@ -1,6 +1,5 @@
 import mongoose from 'mongoose';
 
-//mongoose.connect('mongodb://admin:secret@127.0.0.1:27017/ecomm-product?authSource=admin')
 mongoose.connect('mongodb://admin:secret@mongodb:27017/ecomm-account?authSource=admin')
 const db = mongoose.connection;
 
