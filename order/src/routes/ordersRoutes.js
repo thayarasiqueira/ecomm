5ac51d62dd8c9f59be64df4e017b428fb614c4c4
thayarasiqueira <thay.siqu@gmail.com>
2023-03-08 15:29:08 -0300

    feat: implement token verification on all order's endpoints with bearer passport

--- order/src/routes/ordersRoutes.js
+++ order/src/routes/ordersRoutes.js
@@ -1,11 +1,13 @@
 import express from 'express';
+import passport from 'passport';
 import OrderController from '../controllers/ordersController.js';
 
 const router = express.Router();
+const authenticateBearer = passport.authenticate('bearer', { session: false });
 
 router
-  .get('/orders/:id', OrderController.findOrderById)
-  .post('/admin/orders', OrderController.createOrder)
-  .patch('/admin/orders/:id', OrderController.confirmOrder);
+  .get('/orders/:id', authenticateBearer, OrderController.findOrderById)
+  .post('/admin/orders', authenticateBearer, OrderController.createOrder)
+  .patch('/admin/orders/:id', authenticateBearer, OrderController.confirmOrder);
 
 export default router;
