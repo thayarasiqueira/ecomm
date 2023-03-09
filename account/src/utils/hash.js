5e6652065195ba0f949db93cdf354c1554b6c2a8
thayarasiqueira <thay.siqu@gmail.com>
2023-03-09 08:46:22 -0300

    fix: bcrypt method to decrypt

--- account/src/utils/hash.js
+++ account/src/utils/hash.js
@@ -2,6 +2,6 @@ import bcrypt from 'bcryptjs';
 
 const SALT = bcrypt.genSaltSync(12);
 const encrypt = (password) => bcrypt.hashSync(password, SALT);
-const decrypt = (password, hash) => bcrypt.compareSync(password, hash);
+const decrypt = (password, hash) => bcrypt.compare(password, hash);
 
 export { encrypt, decrypt };
