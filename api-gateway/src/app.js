0b5d4e22590058535200a680521358212e1551e4
thayarasiqueira <thay.siqu@gmail.com>
2023-03-14 16:00:56 -0300

    feat: implement rate limit on all routes

--- api-gateway/src/app.js
+++ api-gateway/src/app.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import rateLimit from 'express-rate-limit';
 import account from './routes/account.js';
 import finance from './routes/finance.js';
 import order from './routes/order.js';
@@ -6,6 +7,12 @@ import category from './routes/category.js';
 import product from './routes/product.js';
 
 const app = express();
+const limiter = rateLimit({
+  windowMs: 1 * 60 * 1000,
+  max: 100,
+});
+
+app.use('/', limiter);
 
 app.use('/', account);
 app.use('/', finance);
