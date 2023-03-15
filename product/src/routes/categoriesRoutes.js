1323f8c2a29aeacf8a23d32ffd8a816e7f2d68c9
thayarasiqueira <thay.siqu@gmail.com>
2023-03-15 08:57:01 -0300

    refactor: migrate token authorization to API Gateway

--- product/src/routes/categoriesRoutes.js
+++ product/src/routes/categoriesRoutes.js
@@ -1,17 +1,15 @@
 import express from 'express';
-import passport from 'passport';
 import CategoryController from '../controllers/categoriesController.js';
 import validateCategory from '../validations/categoriesValidation.js';
 
 const router = express.Router();
-const authenticateBearer = passport.authenticate('bearer', { session: false });
 
 router
   .get('/categories', CategoryController.findCategories)
   .get('/categories/:id', CategoryController.findCategoryById)
-  .post('/admin/categories', authenticateBearer, validateCategory, CategoryController.createCategory)
-  .put('/admin/categories/:id', authenticateBearer, CategoryController.updateCategory)
-  .patch('/admin/categories/:id', authenticateBearer, CategoryController.updateStatusCategory)
-  .delete('/admin/categories/:id', authenticateBearer, CategoryController.deleteCategory);
+  .post('/admin/categories', validateCategory, CategoryController.createCategory)
+  .put('/admin/categories/:id', CategoryController.updateCategory)
+  .patch('/admin/categories/:id', CategoryController.updateStatusCategory)
+  .delete('/admin/categories/:id', CategoryController.deleteCategory);
 
 export default router;
